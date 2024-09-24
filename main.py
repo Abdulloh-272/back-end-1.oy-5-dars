da87ca61afec57121974b90813eb18d66484e759
Abdulloh-272 <167763951+Abdulloh-272@users.noreply.github.com>
2024-09-24 20:54:14 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+ism = input("Ismingizni kiriting: ")
+yosh = input("Yoshingizni kiriting: ")
+manzil = input("Manzilingizni kiriting: ")
+
+
+print(f"Ism: {ism}, Yosh: {yosh}, Manzil: {manzil}")
+
+oxirgi_xarf = ism[-1]
+print("Ismingizning oxirgi xarfi", oxirgi_xarf)
